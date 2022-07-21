ac443a9e32c9815a0bd6c48f0e841bc7b484f6f0
Tasmiya Bano <tbano@tbano-a01.vmware.com>
2022-07-22 00:24:02 +0530

    Part 2 and Part 3: Solution with code refactoring and failing tests implementation

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,15 +18,16 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime current = getCurrentTime();
+        if (current.isAfter(openingTime) && current.isBefore(closingTime))
+            return true;
+        return false;
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
     }
 
     private Item findItemByName(String itemName){
